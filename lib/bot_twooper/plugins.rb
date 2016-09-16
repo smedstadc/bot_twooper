fdfb085469e40057843c3144959f64ef045fde38
Corey Smedstad <smedstadc@gmail.com>
2016-09-16 21:34:57 +0000

    fucking dot

--- lib/bot_twooper/plugins.rb
+++ lib/bot_twooper/plugins.rb
@@ -7,7 +7,7 @@ module BotTwooper
   module Plugins
 
     def self.command_for(event)
-      if event.message.content =~ /\A.(\w+)/
+      if event.message.content =~ /\A\.(\w+)/
         plugins = constants.map { |constant| const_get(constant) }
         result = plugins.find { |plugin| plugin.respond_to?($1) }
         result ? result.method($1) : nil
