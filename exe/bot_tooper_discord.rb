f7fe60bc35e3568c94270054526b5b2994c22ef5
Corey Smedstad <smedstadc@gmail.com>
2016-09-18 19:22:16 -0400

    just an idea

--- exe/bot_tooper_discord.rb
+++ exe/bot_tooper_discord.rb
@@ -8,28 +8,45 @@ if ARGV.include?("--with-dotenv")
   Dotenv.load '.env'
 end
 
+# noinspection RubyArgCount
 bot = Discordrb::Bot.new(token: ENV["DISCORD_BOT_TOKEN"], application_id: ENV["DISCORD_APP_ID"])
 
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
 
-bot.message do |event|
-  command = BotTwooper::Plugins.command_for(event)
-  response = command ? command.call(event) : nil
-  unless response.nil? || response.empty?
-    if response.is_a? Array
-      event << "```\n"
-      response[0...10].each do |line|
-        event << line
+module BotTwooper
+  class Responder
+    def initialize(options={})
+      @responder = options[:responder]
+    end
+
+    def respond_to(event)
+      @responder.respond_to(event)
+    end
+  end
+
+  class DiscordResponder
+    def self.respond_to(event)
+      command = BotTwooper::Plugins.command_for(event)
+      response = command ? command.call(event) : nil
+      unless response.nil? || response.empty?
+        if response.is_a? Array
+          event << "```\n"
+          response[0...10].each do |line|
+            event << line
+          end
+          if response.size > 10
+            event << "...and #{response.size - 10} more results, could you be more specific?"
+          end
+          event << "```"
+        elsif response.is_a? String
+          event.respond "\n`#{response}`"
+        end
       end
-      if response.size > 10
-        event << "...and #{response.size - 10} more results, could you be more specific?"
-      end
-      event << "```"
-    elsif response.is_a? String
-      event.respond "\n`#{response}`"
     end
   end
 end
 
+responder = BotTwooper::Responder.new(responder: BotTwooper::DiscordResponder)
+bot.message {|event| responder.respond_to(event)}
 bot.run
